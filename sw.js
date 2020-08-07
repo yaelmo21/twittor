6a46449d7bd624c573f6eeb416f49d8fc22c6f74
yaelmo <yaelruiz99@outlook.com>
2020-08-07 06:52:40 -0500

    moficiaciones iphonex+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const APP_SHELL = [
     // '/',
@@ -42,6 +42,10 @@ self.addEventListener('activate',e=>{
             if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
+            if (  key !== DYNAMIC_CACHE_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
 
         });
 
