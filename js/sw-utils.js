cf13afe9d03bbc77c01b6b44504fe65dd3b5cd80
yaelmo <yaelruiz99@outlook.com>
2020-08-07 06:16:04 -0500

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+const actualizaCacheDinamico=(dynamicCache,req,res)=>{
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache=>{
+            cache.put(req,res.clone());
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+}
